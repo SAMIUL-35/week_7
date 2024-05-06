8ab0660c69327c5d15401a406c54cf6aa05f77a9
SAMIUL2022 <sami35bd@gmail.com>
2024-05-06 20:42:09 +0600

    .

---
+++ day_3/B_3_palindrome.cpp
@@ -0,0 +1,58 @@
+// B M SAMIUL
+#include <bits/stdc++.h>
+using namespace std;
+#define op()                      \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define yes cout << "YES\n";
+#define no cout << "NO\n";
+#define endl   '\n';
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) ((a * b) / gcd(a, b))
+#define PI 2.0 * acos(0.0)
+#define Dpos(n) fixed << setprecision(n)
+#define lpi(j) for(int i=0; i<n; i++) cin>>j[i]
+#define lpo(j) for(int i=0; i<n; i++) cout<<j[i]<<" ";
+#define so(j) for(j.begin(),j.end()) ;
+#define sog(j)  sort(j.rbegin(), j.rend());
+ 
+typedef long long ll;
+typedef vector<ll> vl;
+const ll N = 1e5 + 1;
+const ll MOD = 1e9 + 7;
+ 
+void sami()
+{
+    ll n;
+    cin >> n;
+    string s;
+    ll a = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        if (a <= 1)
+        {
+            s += 'a';
+            a++;
+        }
+        else if (a <= 3)
+        {
+            s += 'b';
+            a++;
+        }
+        else
+        {
+            // Reset a to 0 to maintain the pattern
+            a = 0;
+            s += 'a';
+            a++;
+        }
+    }
+    cout << s << endl;
+}
+
+int main()
+{
+    op() int tc = 1;
+    sami();
+}
